705bfebb7b20e8b0cb41842ee8a1edd2eda5dd31
Isabelle <155624058+Isabelle36@users.noreply.github.com>
2025-02-17 14:46:16 +0530

    added pomododro and feyman in home

---
+++ frontend-study/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 30s linear infinite',
+        'gradient': 'gradient 8s linear infinite',
+        'pulse': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
